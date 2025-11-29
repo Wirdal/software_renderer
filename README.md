6eb85a580ef0b2cedbea0322116015aded9df759
Chase Maguire <wirdal123@gmail.com>
2025-11-28 19:27:01 -0800

    Matrix basics

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## SDL3
 
 Download and build the static libs for SDL
-Change the makefile vars, SDL3INCLUDE SDL3LIB SDLSTATICNAME, to reflect the path of it
-Change the compiler if you need
+
+Change the makefile vars, SDL3INCLUDE SDL3LIB SDLSTATICNAME to whatever your path is
+
 
 ## Building
+Change the compiler if you need in the makefile
 Run `make main`
--- src/math/matrix.h
+++ src/math/matrix.h
@@ -3,11 +3,20 @@
 #include "concepts/concepts.h"
 #include "evector.h"
 
+// Stored in row-major order
 template <size_t Rows, size_t Cols, Floating T>
 class EMatrix
 {
 public:
-	
+	evector<T, Cols>& operator[] (size_t index)
+	{
+		return m_data[index];
+	}
+
+	const evector<T, Cols>& operator[] (size_t index) const
+	{
+		return m_data[index];
+	}
 private:
-	evector<T, Cols> m_data[Rows];
+	evector<T, Rows> m_data[Cols];
 };
