ad6e571fdd1d9c1a4c3186d143b46dd2bf4f2f0c
Chase Maguire <wirdal123@gmail.com>
2025-12-11 21:03:37 -0800

    I took linear alg a long time ago, ok

--- src/math/matrix.h
+++ src/math/matrix.h
@@ -18,16 +18,28 @@ public:
 		return m_data[index];
 	}
 
-	friend constexpr matrix<Rows, Cols, T> operator*(const matrix<Rows, Cols, T>& lhs, const matrix<Rows, Cols, T>& rhs)
+	
+	// The "rows" of LHS are multiplied into the "columns" of RHS
+	// The output matrix will be of size LHS rows x RHS cols
+	// In general, an element of the output matrix is the dot product of row I from LHS and column J from RHS
+	friend constexpr matrix<Rows, Cols, T> operator*(const matrix<Rows, Cols, T>& lhs, const matrix<Cols, Rows, T>& rhs)
 	{
 		matrix<Rows, Cols, T> result{};
-
+		
+		// For each elem in result matrix
 		for (size_t row = 0; row < Rows; row++)
 		{
 			for (size_t col = 0; col < Cols; col++)
 			{
-				// Matrix product is the dot between each row of lhs and each column of rhs
-				T dot_prod = lhs[row].dot(rhs[col]);
+				T dot_prod{};
+				// For each dataum in the inputs
+				for (size_t inner = 0; inner < Rows; inner++)
+				{
+					// Matrix product is the dot between each row of lhs and each column of rhs
+					// Since these are both represented in row-major order, we cannot just _dot_ each
+					dot_prod += lhs[row][inner] * rhs[inner][col];
+				}
+
 				result[row][col] = dot_prod;
 			}
 		}
