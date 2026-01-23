cba4df445c589c11d48a40a3b757c6eb5afa94f6
Chase Maguire <wirdal123@gmail.com>
2026-01-22 22:40:55 -0800

    Rotation about y

--- tests/matrix_tests.cpp
+++ tests/matrix_tests.cpp
@@ -141,7 +141,6 @@ TEST_SUITE(matrix_tests)
 	{
 		translation_matrix<float> mat = matrix<4, 4, float>::identity();
 
-
 		evector<float, 4> before{1.0f, 2.0f, 3.0f, 1.0f};
 
 		// Rotation about x axis with angel phi
@@ -153,11 +152,27 @@ TEST_SUITE(matrix_tests)
 
 		matrix<1, 4, float> after = before.transpose() * mat;
 
-
-		// We're rotating about x, but not far enough to inverse the Z coord
 		constexpr evector<float, 4> afterXCheck{1.0f, -2.0f, -3.0f, 1.0f};
 		CHECK_TRUE(after == afterXCheck.transpose());
+	}
+	
+	// Rotation about Y
+	{
+		translation_matrix<float> mat = matrix<4, 4, float>::identity();
 
+		evector<float, 4> before{1.0f, 2.0f, 3.0f, 1.0f};
+
+		// Rotation about y axis with angel phi
+		constexpr float pi = std::numbers::pi_v<float>;
+		mat[0][0] = std::cos(pi);
+		mat[0][2] = -std::sin(pi);
+		mat[2][0] = std::sin(pi);
+		mat[2][2] = std::cos(pi);
+
+		matrix<1, 4, float> after = before.transpose() * mat;
+
+		constexpr evector<float, 4> afterXCheck{-1.0f, 2.0f, -3.0f, 1.0f};
+		CHECK_TRUE(after == afterXCheck.transpose());
 	}
 }
 TEST_SUITE_END
